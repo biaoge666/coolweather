a6271da85db4a5a483680baf3525d875d0d7421c
biaoge666 <1258831159@qq.com>
2017-09-26 19:25:04 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/biaoge666/coolweather/db/City.java
@@ -0,0 +1,43 @@
+package com.biaoge666.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 汤洪 on 2017/9/26.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getCityName(){
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/biaoge666/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.biaoge666.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 汤洪 on 2017/9/26.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+}
